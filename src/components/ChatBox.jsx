7ca1acb87cdce4b2157718c06425c7651097bb3f
Michael-12798 <backiarajranjithmichael@gmail.com>
2026-08-25 11:59:43 +0530

    updated

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -17,7 +17,7 @@ const ChatBox = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://cb-backend-3xii.onrender.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
